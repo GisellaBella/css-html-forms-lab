97912666f083097cda681843f635fae718de8fb8
Gisellabella <gisella@gmail.com>
2016-11-15 09:05:24 -0700

    fixed radio buttons.

---
+++ starter_code/flower.js
@@ -0,0 +1,28 @@
+function Flower (color, petals, smells){
+this.color = color;
+this.petals= petals;
+this.smells = smells;}
+
+var snapDragon = ("pink", 12, TRUE);
+
+
+
+
+
+function SnapDragon (){
+this.color = "pink";
+this.petals= 32;
+this.smells = true;}
+
+
+Function Flower() {
+    this.color = "red";
+    this.petals = 32;
+    this.smells= true;
+    this.dead = function () {
+    	console.log("you forgot to water me!");
+    
+}
+
+
+function.prot
